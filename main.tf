e769b1329dc3a90069f7fbd22bee3d203cc1dd9a
Altieres Freitas <altieressfreitas@gmail.com>
2019-09-19 02:27:05 -0300

    Add port and security groups

--- main.tf
+++ main.tf
@@ -1,25 +1,36 @@
 locals {
-  cloud_init_count = var.user_data_template == "" ? 0 : var.instance_count
-  port_count = var.port == {} ? 0 : var.instance_count
+  template_count = lookup(var.cloud_init, "template",  null ) == null ? 0 : var.instance_count
+  template_vars = lookup(var.cloud_init, "vars", {})
+  port_count       = length(var.port) == 0 ? 0 : var.instance_count
 
 }
 
 data "template_file" "cloud_init" {
-  count    = local.cloud_init_count
-  template = templatefile( var.user_data_template, merge(var.user_data_variables, { hostname = format("%s_%s", var.name, count.index + 1 ) }))
+  count = local.template_count
+  template = templatefile(var.cloud_init.template,
+    merge(
+      local.template_vars,
+      {
+        hostname       = format("%s_%d", var.name, count.index + 1),
+        index          = count.index,
+        all_fixed_ips = flatten(openstack_networking_port_v2.port.*.all_fixed_ips)
+      }
+  ))
 }
 
 
 resource "openstack_networking_port_v2" "port" {
-  count          = local.port_count
-  network_id     = var.port.network_id
-  admin_state_up = "true"
+  count              = local.port_count
+  network_id         = var.port.network_id
+  security_group_ids = var.security_group_ids
+  admin_state_up     = "true"
 
   dynamic "allowed_address_pairs" {
-
-    for_each = var.port.allowed_address_pairs
+    iterator = port
+    for_each = flatten(list(var.port))
     content {
-      ip_address = format("%s", allowed_address_pairs.value)
+      ip_address  = lookup(port.value.allowed_address_pairs, "ip_address", null)
+      mac_address = lookup(port.value.allowed_address_pairs, "mac_address", null)
     }
   }
 }
@@ -28,21 +39,31 @@ resource "openstack_networking_port_v2" "port" {
 
 resource "openstack_compute_instance_v2" "instance" {
   count               = var.instance_count
-  name                = format("%s_%d", var.name, count.index + 1 )
+  name                = format("%s_%d", var.name, count.index + 1)
   image_name          = var.image
   flavor_name         = var.flavor
-  user_data           = element(data.template_file.cloud_init.*.rendered,count.index)
+  user_data           = element(data.template_file.cloud_init.*.rendered, count.index)
   stop_before_destroy = var.stop_before_destroy
-  security_groups     = var.security_groups
+  security_groups     = (var.security_group_ids == null || length(var.port) == 0) ? var.security_groups : null
 
-  metadata = var.metadata
+  metadata = var.tags
 
-  dynamic "network"{
-    for_each = var.network
+  dynamic "network" {
+    for_each = var.networks
     content {
-     name = network.value
+      name = lookup(network.value, "name", null)
+      uuid = lookup(network.value, "uuid", null)
     }
   }
+
+  dynamic "network" {
+    iterator = port
+    for_each = flatten(list(var.port))
+    content {
+      port = element(openstack_networking_port_v2.port.*.id, count.index)
+    }
+  }
+
 
   lifecycle {
     ignore_changes = []
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "instance_count" {
 
 variable "name" {
   description = "Name to be used on all resources as prefix"
-  default = "instance_name"
+  default     = "instance_name"
 }
 
 variable "image" {
@@ -16,38 +16,40 @@ variable "flavor" {
   description = "The Instance Flavor"
 }
 
-variable "network" {
-  description = "The network used to attach the instance"
+variable "networks" {
+  description = "The network list used to attach the instance"
+  default     = []
+  type        = list
 }
 
 variable "port" {
   description = "The config to define create_port"
-  default = []
+  default     = []
 }
 
-variable "user_data_template" {
-  description = "user_data_template file used in instance"
-  default = ""
+variable "cloud_init" {
+  description = "cloud_init is a map containing template and variables to render"
+  default     = {}
 }
 
-variable "user_data_variables" {
-  description = "user_data_variables used to render template_file "
-  default = {}
+variable "tags" {
+  description = "tags map to use on instance metadata"
+  default     = {}
 }
 
-variable "metadata" {
-  description = "metadata map to use on instance create"
-  default = {}
+variable "security_group_ids" {
+  description = "security_group_ids list to associate with instance"
+  default     = null
 }
 
 variable "security_groups" {
   description = "security_groups list to associate with instance"
-  default = ["default"]
+  default     = null
 }
 
 variable "stop_before_destroy" {
   description = "Boolean to define if instance needs stop before destroy"
-  default = false
+  default     = false
 }
 
 
